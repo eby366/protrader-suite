980caa0ca0e850c6b5cdd08a84e6cbc2e546bc5e
eby366 <kpoleclement@gmail.com>
2026-04-06 22:37:42 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'protrader-v5';
-const ASSETS = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { if (e.request.url.includes('api.') || e.request.url.includes('onrender.com')) return; e.respondWith(fetch(e.request).then(r => { caches.open(CACHE).then(c => c.put(e.request, r.clone())); return r; }).catch(() => caches.match(e.request))); });
